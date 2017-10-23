ac2951254408ee7ffdde71f11b735e4dc0b855ab
Alex Persaud <alex_persaud01@yahoo.com>
2017-10-23 16:14:56 -0400

    Add layout for user sign up and create new parse user

--- app/src/main/java/com/alexpersaud/instagramclone/MainActivity.java
+++ app/src/main/java/com/alexpersaud/instagramclone/MainActivity.java
@@ -2,17 +2,48 @@ package com.alexpersaud.instagramclone;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
 
 import com.parse.ParseAnalytics;
+import com.parse.ParseException;
+import com.parse.ParseUser;
+import com.parse.SignUpCallback;
 
 public class MainActivity extends AppCompatActivity {
+
+    public void signUp(View view){
+        EditText username = (EditText) findViewById(R.id.username);
+        EditText password = (EditText) findViewById(R.id.password);
+        if(username.getText().toString().matches("") || password.getText().toString().matches("")){
+            Toast.makeText(this, "A username and password are required.", Toast.LENGTH_SHORT).show();
+        }else{
+            ParseUser user = new ParseUser();
+            user.setUsername(username.getText().toString());
+            user.setPassword(password.getText().toString());
+
+            user.signUpInBackground(new SignUpCallback() {
+                @Override
+                public void done(ParseException e) {
+                    if(e == null){
+                        Log.i("Sign Up", "Successful!");
+                    }else{
+                        Toast.makeText(MainActivity.this, e.getMessage(), Toast.LENGTH_SHORT).show();
+                    }
+                }
+            });
+        }
+
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-
         ParseAnalytics.trackAppOpenedInBackground(getIntent());
     }
+
 }
